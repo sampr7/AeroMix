fc1f5d434dce51cd5a5ef50ef2774e13a1dcead9
Sam P Raj <65779808+sampr7@users.noreply.github.com>
2023-11-26 21:10:11 +0530

    Delete aeromix directory

--- aeromix/__init__.py
+++
@@ -1,4 +0,0 @@
-from aeromix.AeroMix_main import run
-from aeromix.getAerosolType import getAerosolType
-from aeromix.CopyAerosolData import CopyAerosolData
-from aeromix.newAerosol import ext_aerosol,cs_aerosol,getSampleInputDict_ext,getSampleInputDict_cs
