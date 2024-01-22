c741e7d33c63529a9ea23f0093a18d0a6196808a
Sam P Raj <sampr7@gmail.com>
2024-01-22 08:52:37 +0530

    AeroMix 1.0.1

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-
-"""
-from setuptools import setup
+"""Setup file."""
 from pathlib import Path
+from setuptools import setup
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
       name='AeroMix',
-      version='1.0.0',
+      version='1.0.1',
       description='Python package for modeling aerosol optical properties',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -21,13 +17,13 @@ setup(
       packages=['AeroMix'],
       package_dir={'AeroMix': 'AeroMix'},
       classifiers=[
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering :: Atmospheric Science',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python :: 3',
-    'Operating System :: OS Independent',],
-    keywords=['aerosol model mixing state AOD SSA g'],
-    install_requires=['numpy','scipy','PyMieScatt'],
-    package_data={'AeroMix':['aerosol_components/*']},
-    zip_safe=False)
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Atmospheric Science',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3',
+          'Operating System :: OS Independent', ],
+      keywords=['aerosol model mixing state AOD SSA g'],
+      install_requires=['numpy', 'scipy', 'PyMieScatt'],
+      package_data={'AeroMix': ['aerosol_components/*']},
+      zip_safe=False)
